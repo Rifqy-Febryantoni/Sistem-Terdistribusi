c2fb4b3f8e7dbe0e7ee9dcc0b0a3c8584d47bef3
Rifqy_Febryantoni <m.rifqyfebryantoni@gmail.com>
2025-11-11 11:03:03 +0700

    change test

--- mutex/testing/test1.sh
+++ mutex/testing/test1.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-#
 
-# Single PUT to node 1
-python3 ../program/kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 1  "PUT color blue"
+# Variabel NODES baru untuk 4 peer GNS3 Anda
+NODES="192.168.122.11:8001,192.168.122.12:8002,192.168.122.13:8003,192.168.122.14:8004"
+
+echo "--- [TEST 1: Single PUT to Node 0 (pc-2)] ---"
+# Single PUT to node 1 (index 0)
+python3 ../program/kvclient.py --nodes $NODES cmd --node 0 "PUT color blue"
 
 
-# GET from node 2
-python3 ../program/kvclient.py  --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 2   "GET color"
+echo "--- [TEST 2: Single GET from Node 1 (pc-3)] ---"
+# GET from node 2 (index 1)
+python3 ../program/kvclient.py --nodes $NODES cmd --node 1 "GET color"
 
 
+echo "--- [TEST 3: MEMICU RACE CONDITION] ---"
 # Race two writers (great for no-mutex demo)
-python3 ../program/kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 race  "PUT color blue" "PUT color red"
+python3 ../program/kvclient.py --nodes $NODES race "PUT color blue" "PUT color red"
 
+
+echo "--- [TEST 4: MEMERIKSA KONSISTENSI DATA] ---"
+echo "--- [Menunggu 1 detik untuk replikasi...] ---"
+# Jeda ini PENTING untuk GNS3 agar replikasi selesai sebelum dicek
+sleep 1
 
 # Read the key from ALL nodes after the race
-python3 ../program/kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003  getall color
-
-
-
-
+python3 ../program/kvclient.py --nodes $NODES getall color
--- mutex/testing/test2.sh
+++ mutex/testing/test2.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-#
 
-# Single PUT to node 1
-python3 ../program/kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 1  "PUT color red"
-python3 ../program/kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 2  "PUT color blue"
+# Variabel NODES baru untuk 4 peer GNS3 Anda
+NODES="192.168.122.11:8001,192.168.122.12:8002,192.168.122.13:8003,192.168.122.14:8004"
 
 
-# GET from node 2
-python3 ../program/kvclient.py  --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 2   "GET color"
-python3 ../program/kvclient.py  --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 0   "GET color"
+echo "--- [TEST 1: PUT 'red' ke Node 1 (pc-3)] ---"
+# Single PUT to node 1 (index 1)
+python3 ../program/kvclient.py --nodes $NODES cmd --node 1 "PUT color red"
+
+echo "--- [TEST 2: PUT 'blue' ke Node 2 (pc-4)] ---"
+# Single PUT to node 2 (index 2)
+python3 ../program/kvclient.py --nodes $NODES cmd --node 2 "PUT color blue"
 
 
+# Beri waktu untuk replikasi
+sleep 1
 
 
+echo "--- [TEST 3: GET dari Node 2 (pc-4)] ---"
+# GET from node 2 (index 2)
+python3 ../program/kvclient.py --nodes $NODES cmd --node 2 "GET color"
+
+echo "--- [TEST 4: GET dari Node 0 (pc-2)] ---"
+# GET from node 0 (index 0)
+python3 ../program/kvclient.py --nodes $NODES cmd --node 0 "GET color"
